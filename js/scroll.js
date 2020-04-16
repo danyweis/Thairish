b4a84cb206a7428c88b11f52752d3acb194acea3
danyweis <danyweismail@gmail.com>
2020-04-16 19:47:40 +0100

    finished the navbar and finished the layout of mobile

--- js/scroll.js
+++ js/scroll.js
@@ -6,12 +6,14 @@ const pic2 = document.querySelector('.picture2');
 const width = window.innerWidth;
 
 
+
+
 window.onscroll = function () {
     let scrolled = window.scrollY;
 
     //  NAVBAR BACKGROUND
 
-    if (scrolled >= 100) {
+    if (scrolled >= 100 && width >= 720) {
         navbar.classList.add('active')
     } else {
         navbar.classList.remove('active')
@@ -20,21 +22,51 @@ window.onscroll = function () {
     //  ABOUT EASE IN 
 
     if (width > 960 && scrolled >= height / 3) {
-        aboutHidden.classList.add('aboutText')    
-    } else if(width <= 960 && width > 720 && scrolled >= height/2){
+        aboutHidden.classList.add('aboutText')
+    } else if (width <= 960 && width > 720 && scrolled >= height / 2) {
+        aboutHidden.classList.add('aboutText')
+    } else if(scrolled >= 200){
         aboutHidden.classList.add('aboutText')
     }
 
-    if(width > 960 && scrolled >= height/2) {
+    if (width > 960 && scrolled >= height / 2) {
         pic1.classList.add('picture')
-    } else if(width <= 960 && width > 720 && scrolled >= 150){
+    } else if (width <= 960 && width > 720 && scrolled >= 150) {
         pic1.classList.add('picture3')
         pic2.classList.add('picture')
-    } 
+    } else if (scrolled >= 100){
+        pic1.classList.add('picture')
+    }
 
-    if(width > 960 && scrolled >= height) {
+    if (width > 960 && scrolled >= height) {
         pic2.classList.add('picture')
     }
 
 }
 
+
+const navSlide = () => {
+    const burgerBox = document.querySelector('.burgerBox');
+    const linkList = document.querySelector('.linkList');
+    const link = document.querySelectorAll('.linkList li a');
+
+    burgerBox.addEventListener('click', function () {
+
+        linkList.classList.toggle('navActive')
+
+        link.forEach(function (x, index) {
+            if (x.style.animation) {
+                x.style.animation = "";
+            } else {
+                x.style.animation = `linkMove 0.5s ease-in forwards ${index / 5 + 0.2}s`
+            }
+        });
+
+        burgerBox.classList.toggle('burger');
+
+    });
+};
+
+
+
+navSlide();
